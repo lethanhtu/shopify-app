cf7b221c6e26510d1be5fb27e88c451eb5bc7f92
Tu Le <t.le@shopmacher.de>
2019-03-04 17:04:18 +0700

    fix

--- src/Controller/AppSliderController.php
+++ src/Controller/AppSliderController.php
@@ -4,22 +4,31 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class AppSliderController extends AbstractController
 {
-    /**
-     * @Route("/app/slider/install", name="app_slider_install")
-     */
-    public function install()
-    {
 
+    const URL = "https://shopify-app-tule.herokuapp.com/";
+    const ApiKey = "2a5fd5fb378d4dc66a675342ee712c56";
+
+    /**
+     * @Route("/slider/install", name="app_slider_install")
+     */
+    public function install(Request $request)
+    {
+        $shop = "https://".$request->get('shop');
+        $scopes = "read_orders,read_products,write_products";
+        $url = $shop."/admin/oauth/request_grant?client_id=".self::ApiKey."&scope=".$scopes."&redirect_uri=".self::URL."/slider/auth";
+        return new RedirectResponse($url);
     }
 
     /**
-     * @Route("/app/slider/auth", name="app_slider_install_auth")
+     * @Route("/slider/auth", name="app_slider_install_auth")
      */
     public function auth()
     {
-        
+
     }
 }
