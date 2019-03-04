d83c1a5aa8f3b3121a4339a20a64ef24a8a93f06
Tu Le <t.le@shopmacher.de>
2019-03-04 16:28:56 +0700

    add-shopify

---
+++ src/Controller/AppSliderController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AppSliderController extends AbstractController
+{
+    /**
+     * @Route("/app/slider/install", name="app_slider_install")
+     */
+    public function install()
+    {
+
+    }
+
+    /**
+     * @Route("/app/slider/auth", name="app_slider_install_auth")
+     */
+    public function auth()
+    {
+        
+    }
+}
