33f2d109dd7d150ed851ae49eb7da6e0fc67cc4c
Tu Le <t.le@shopmacher.de>
2019-03-05 11:07:07 +0700

    fix

--- src/Controller/AppSliderController.php
+++ src/Controller/AppSliderController.php
@@ -11,6 +11,7 @@ class AppSliderController extends AbstractController
 
     const URL = "https://shopify-app-tule.herokuapp.com";
     const ApiKey = "2a5fd5fb378d4dc66a675342ee712c56";
+    const ApiSecret = "ebf6f972200bcaeaefd7033fb064f297";
 
     public function install(Request $request)
     {
@@ -21,8 +22,30 @@ class AppSliderController extends AbstractController
     }
 
 
-    public function auth()
+    public function auth(Request $request)
     {
+        $hmac = $_GET['hmac'];
+        unset($_GET['hmac']);
 
+        foreach($_GET as $key=>$value){
+
+            $key=str_replace("%","%25",$key);
+            $key=str_replace("&","%26",$key);
+            $key=str_replace("=","%3D",$key);
+            $value=str_replace("%","%25",$value);
+            $value=str_replace("&","%26",$value);
+
+            $ar[] = $key."=".$value;
+        }
+
+        $str = join('&',$ar);
+        $ver_hmac =  hash_hmac('sha256',$str,self::ApiSecret,false);
+
+        if($ver_hmac==$hmac)
+        {
+            echo 'hmac verified';
+        } else {
+            echo 'fail';
+        }
     }
 }
