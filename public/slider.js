877d72371bf2d47e6a561aebfc19799b880a10f2
Tu Le <t.le@shopmacher.de>
2019-03-07 01:39:54 +0700

    fix

--- public/slider.js
+++ public/slider.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
-        loop:true,
-        margin:10,
+        loop:false,
+        margin:20,
         nav:true,
         responsive:{
             0:{
