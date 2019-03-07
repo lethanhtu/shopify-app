ff76dbc7f5599df0ec5ef229cc7cb89fbe83d5d4
SoaJenny <s.nguyen@shopmacher.de>
2019-03-07 10:58:16 +0700

    change-lib

--- public/slider.js
+++ public/slider.js
@@ -1,18 +1,1 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-        loop:false,
-        margin:20,
-        nav:true,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:3
-            },
-            1000:{
-                items:6
-            }
-        }
-    });
-});
+alert(1);
