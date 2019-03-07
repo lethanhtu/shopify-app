f14f665c19f43571d8f4d976ab47671525c5b245
SoaJenny <s.nguyen@shopmacher.de>
2019-03-07 11:10:10 +0700

    change

--- public/slider.js
+++ public/slider.js
@@ -1,8 +1,19 @@
 $(document).ready(function(){
   $('.your-class').slick({
-    slidesToShow: 3,
+    slidesToShow: 5,
     slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
+    autoplay: false,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: false,
+          dots: true
+        }
+      }
+    ]
   });
 });
