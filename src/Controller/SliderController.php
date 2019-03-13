806842273d4a60e9060937c5287db5c031966f8c
Tu Le <t.le@shopmacher.de>
2019-03-13 15:25:11 +0700

    refactor

---
+++ src/Controller/SliderController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use GuzzleHttp\Client;
+use App\Service\Shopify\ShopifyUtil;
+use App\Service\Shopify\ShopifyAuth;
+
+
+class AppSliderController extends AbstractController
+{
+
+    public function install()
+    {
+        $shopUrl = ShopifyUtil::getShopURL();
+        $scopes = "read_themes,write_themes";
+
+        $url = sprintf(
+            '%s/admin/oauth/request_grant?client_id=%s&scope=%s&redirect_uri=%s/slider/auth',
+            $shopUrl,
+            getenv('API_KEY'),
+            $scopes,
+            getenv('APP_URL')
+        );
+
+        return new RedirectResponse($url);
+    }
+
+
+    public function auth()
+    {
+        if(ShopifyAuth::validateHMAC()) {
+            return new Redirect('/slider/config');
+        }
+
+        return $this->render('error/500.html.twig');
+    }
+}
