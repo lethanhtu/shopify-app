b5ae46fa0026d6a5f20e8a58779f8912cb6a846e
Tu Le <t.le@shopmacher.de>
2019-03-14 01:37:41 +0700

    fix

---
+++ src/Controller/CustomerController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class CustomerController extends AbstractController
+{
+    public function customerRequest()
+    {
+
+    }
+
+    public function customerErasure()
+    {
+
+    }
+
+    public function shopErasure()
+    {
+
+    }
+}
