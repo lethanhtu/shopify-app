77c47a36daf37a35cd108a492020f4033718122c
Le Thanh Tu <lethanhtu401@gmail.com>
2019-03-27 22:28:30 +0700

    Update CustomerController.php

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -3,22 +3,22 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
+use Symfony\Component\HttpFoundation\Response;
 
 class CustomerController extends AbstractController
 {
     public function customerRequest()
     {
-
+        return new Response();
     }
 
     public function customerErasure()
     {
-
+        return new Response();
     }
 
     public function shopErasure()
     {
-
+        return new Response();
     }
 }
