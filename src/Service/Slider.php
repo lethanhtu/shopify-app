65fe19ae1716a81b6873b477ecebfdfeff61b3a2
Tu Le <t.le@shopmacher.de>
2019-03-27 11:49:25 +0700

    fix-js

--- src/Service/Slider.php
+++ src/Service/Slider.php
@@ -36,14 +36,51 @@ class Slider
      */
     public function addContent($sliderId)
     {
-        $indexContent = $this->request->getTemplateContent('templates/index.liquid');
-        $indexContent.= sprintf(
-            '<div id="shopmacher-slider" slider-id="%s"><script src="%s/slider/embbed.js" defer></script></div>',
+        $productContent = $this->request->getTemplateContent('templates/product.liquid');
+        $productContent.= sprintf(
+            '{% comment %}
+  Start  Product Shopmacher Slider
+{% endcomment %}<div id="shopmacher-slider" slider-id="%s"><script src="%s/slider/embed.js" defer></script></div>',
             $sliderId,
             getenv('APP_URL')
         );
 
-        $this->request->updateTemplateContent('templates/index.liquid', $indexContent);
+        $productContent.='
+<div id="shopmacher-slider-content" class="recommendation-items-block" style="display:none">
+  <div class="page-width">
+    <h2 class="heading">
+      DIE BESTSELLER
+    </h2>
+      {% assign exist_id = "" %}
+    <div class="recommendation-items">
+        {% for collection in product.collections %}
+            {% for product in collection.products %}
+                {% if exist_id contains product.id %}
+                {% else %}
+                  <div class="item">
+                    <a href="{{ product.url }}">
+              <span class="thumb">
+                <img src="{{ product | img_url }}"/>
+              </span>
+                      <span class="content">
+                <div class="name-pd"></div>
+                <div class="short-desc"></div>
+                <div class="price-pd">{{product.price}} $</div>
+              </span>
+                    </a>
+                  </div>
+                    {% assign exist_id = exist_id | append: product.id %}
+                {% endif %}
+            {% endfor %}
+        {% endfor %}
+    </div>
+  </div>
+</div>
+{% comment %}
+  End  Product Shopmacher Slider
+{% endcomment %}';
+
+        $this->request->updateTemplateContent('templates/product.liquid', $productContent);
     }
 
     public function uninstall($shopDomain)
@@ -51,6 +88,7 @@ class Slider
         $shop = $this->em->getRepository(Shop::class)->findOneBy(['shop_id' => $shopDomain]);
         $shop->setActive(0);
         $shop->setUpdatedDate(new \DateTime());
+        $shop->setConfig(null);
         $this->em->persist($shop);
         $this->em->flush();
     }
